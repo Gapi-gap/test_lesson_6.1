1561d326551a318de1227e1057566ce12a6060b0
artem <artem.gaponov.2024@mail.ru>
2025-10-19 18:48:05 +0300

    Добавлено задание

---
+++ src/main/java/ru/netology/stats/infostatistics/Statistics.java
@@ -0,0 +1,66 @@
+package ru.netology.stats.infostatistics;
+
+public class Statistics {
+    public long TotalSales(int[] sales){
+        long sumSales = 0;
+        for(int i =0; i<sales.length;++i){
+            sumSales += sales[i];
+        }
+        return sumSales;
+    }
+
+    public long AverageSales(int[] sales){
+        long sumSales = 0;
+        for(int i =0; i<sales.length;++i){
+            sumSales += sales[i];
+        }
+        return (int)sumSales/sales.length;
+    }
+    public int PeakSales(int[] sales){
+        long max = Long.MIN_VALUE;
+        int mother = -1;
+        for(int i =0; i<sales.length;++i){
+           if(max < sales[i]){
+               max = sales[i];
+               mother  = i;
+           }
+        }
+        return mother+1;
+    }
+
+    public int MinimumSales(int[] sales){
+        long min = Long.MAX_VALUE;
+        int mother =0;
+        for(int i =0; i<sales.length;++i){
+            if(min >= sales[i]){
+                min = sales[i];
+                mother = i;
+            }
+        }
+        return mother+1;
+    }
+
+    public int NumberMonthsAboveAverage(int[] sales){
+      long average = AverageSales(sales);
+      int count = 0;
+        for(int i =0; i<sales.length;++i){
+            if(average < sales[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int NumberMonthsLessThatAverage(int[] sales){
+        long average = AverageSales(sales);
+        System.out.println(average);
+        int count = 0;
+        for(int i =0; i<sales.length;++i){
+            if(average > sales[i]){
+                count++;
+
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/StatisticsTest.java
@@ -0,0 +1,59 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import ru.netology.stats.infostatistics.Statistics;
+public class StatisticsTest {
+    @Test
+    public void GeneralTestForTheSumOfAllSales(){
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,33,44,55,66,11,15,35,64,34,25,75};
+        long sum = objStatistics.TotalSales(sales);
+        int expected = 479;
+        Assertions.assertEquals(expected,sum);
+    }
+
+    @Test
+    public void GeneralTestForTheAverageOfAllSales()
+    {
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,33,44,55,66,11,15,35,64,34,25,75};
+        long sum = objStatistics.AverageSales(sales);
+        double expected = 39;
+        Assertions.assertEquals(expected,sum);
+    }
+    @Test
+    public void MaximumSalesInTheMonth()
+    {
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,75,44,55,33,11,15,35,64,75,25,75};
+        int index = objStatistics.PeakSales(sales);
+        double expected = 2;
+        Assertions.assertEquals(expected,index);
+    }
+    @Test
+    public void MinimumSalesInTheMonth()
+    {
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,33,44,55,66,11,15,35,64,34,25,11};
+        long index = objStatistics.MinimumSales(sales);
+        double expected = 12;
+        Assertions.assertEquals(expected,index);
+    }
+    @Test
+    public void TheNumberTheMonthsIsGreaterThanAverage()
+    {
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,33,44,55,66,11,15,35,64,34,25,75};
+        long count = objStatistics.NumberMonthsAboveAverage(sales);
+        double expected = 5;
+        Assertions.assertEquals(expected,count);
+    }
+    @Test
+    public void NumberOfMonthsLessThanAverage()
+    {
+        Statistics objStatistics = new Statistics();
+        int[] sales = {22,33,44,55,66,11,15,35,64,34,25,75};
+        long count = objStatistics.NumberMonthsLessThatAverage(sales);
+        double expected = 7;
+        Assertions.assertEquals(expected,count);
+    }
+}
